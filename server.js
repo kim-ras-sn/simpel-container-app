e4ad4229363776867096b100878a8080f07849fe
Kim <36648257+kim-ras@users.noreply.github.com>
2022-04-22 16:02:07 +0200

    small fixes. V 1.0 is done

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ app.use("/styles",express.static(__dirname + "/styles"));
 app.use("/images",express.static(__dirname + "/images"));
 
 app.listen(port, () => {
-console.log('listening for request on port ${port}');
+console.log('listening for request on port %s',port);
 });
 
 app.get('/', (req, res) => {
-console.log('root request made');
-res.sendFile("./http/index.html", { root: __dirname })
+    console.log('Root request made');
+    res.sendFile("./http/index.html", { root: __dirname })
 });
 
 app.get('/about', (req, res) => {
     console.log('About request made');
-res.sendFile('./http/about.html', { root: __dirname });
+    res.sendFile('./http/about.html', { root: __dirname });
 });
 
 app.get('/about-us', (req, res) => {
-res.redirect('/about');
+    res.redirect('/about');
 });
 
 app.use((req, res) => {
